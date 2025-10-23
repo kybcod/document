129718ade70a7c3951b5394796abfd4ee1771f63
김예빈 <kyb-0215@naver.com>
2025-10-23 17:15:28 +0900

    서버 기준으로 설정 수정

--- src/main/java/document/DocumentApplication.java
+++ src/main/java/document/DocumentApplication.java
@@ -2,11 +2,18 @@ package document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class DocumentApplication {
+public class DocumentApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DocumentApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DocumentApplication.class, args);
